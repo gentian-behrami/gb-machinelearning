1060e6b601765e5b6eb8fcbce2eb44e57b96eaf7
gentian-behrami <64558032+gentian-behrami@users.noreply.github.com>
2025-11-22 22:37:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,11 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
+
+
+
+
