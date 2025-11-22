dde27517c1978695df9b900c2b134eafb1f0ce1d
gentian-behrami <64558032+gentian-behrami@users.noreply.github.com>
2025-11-22 23:00:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,17 @@ with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
 
+# Data preparations
+with st.sidebar:
+  st.header('Input features')
+
+
+
+
+
+
+
+
+
+
+
